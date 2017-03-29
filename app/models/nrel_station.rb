8893f31f134b28516b0ce22aca8ffd56bc4ccb32
Katy Farmer <dreadpiratekaty@gmail.com>
2017-03-29 10:01:11 -0600

    spiking ideas about formatting response

---
+++ app/models/nrel_station.rb
@@ -0,0 +1,7 @@
+class NrelStation
+  attr_reader :data
+
+  def initialize(data)
+    @data = data
+  end
+end
--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,15 +1,25 @@
 class NrelService
+  attr_reader :client, :token
 
-  def initialize(token)
+  def initialize(token, options={})
     @token = token
+    @client = options.fetch(:client) do
+      Faraday.new(url: "http://developer.nrel.gov/api")
+    end
   end
 
   def nearest_stations(zip)
-    Faraday.get("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{zip}&limit=10")
+    get_data("/alt-fuel-stations/v1/nearest.json?location=#{zip}&limit=10").map do |data|
+      NrelStation.new(data)
+    end
   end
 
   private
-  def client_url
-    Faraday.get("http://developer.nrel.gov/api/alt-fuel-stations/v1")
+  def get_data(path)
+    parse(client.get(path, {access_token: token} ))
+  end
+
+  def parse(response)
+    JSON.parse(response.body)
   end
 end
